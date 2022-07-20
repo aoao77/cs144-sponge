e474ea938d44b284e8ef5906d18c8f0b074b7941
Ainsley <vhtmscyo@gmail.com>
2022-07-20 20:40:52 +0800

    update(webget.cc/get_URL): finish get_URL.

--- apps/webget.cc
+++ apps/webget.cc
@@ -7,8 +7,6 @@
 using namespace std;
 
 void get_URL(const string &host, const string &path) {
-    // Your code here.
-
     // You will need to connect to the "http" service on
     // the computer whose name is in the "host" string,
     // then request the URL path given in the "path" string.
@@ -16,9 +14,20 @@ void get_URL(const string &host, const string &path) {
     // Then you'll need to print out everything the server sends back,
     // (not just one call to read() -- everything) until you reach
     // the "eof" (end of file).
-
-    cerr << "Function called: get_URL(" << host << ", " << path << ").\n";
-    cerr << "Warning: get_URL() has not been implemented yet.\n";
+    TCPSocket socket;
+    socket.connect(Address(host, "http"));
+    // message template
+    string messages;
+    messages += "GET " + path + " HTTP/1.1\r\n";
+    messages += "Host: " + host + "\r\n";
+    messages += "Connection: close\r\n\r\n";
+    // write message
+    socket.write(messages);
+    cout << "Message: \r" << messages;
+    while (!socket.eof()) {
+        cout << socket.read();
+    }
+    socket.close();
 }
 
 int main(int argc, char *argv[]) {
@@ -27,9 +36,9 @@ int main(int argc, char *argv[]) {
             abort();  // For sticklers: don't try to access argv[0] if argc <= 0.
         }
 
-        // The program takes two command-line arguments: the hostname and "path" part of the URL.
-        // Print the usage message unless there are these two arguments (plus the program name
-        // itself, so arg count = 3 in total).
+        // The program takes two command-line arguments: the hostname and "path"
+        // part of the URL. Print the usage message unless there are these two
+        // arguments (plus the program name itself, so arg count = 3 in total).
         if (argc != 3) {
             cerr << "Usage: " << argv[0] << " HOST PATH\n";
             cerr << "\tExample: " << argv[0] << " stanford.edu /class/cs144\n";
