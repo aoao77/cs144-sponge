7f473795a9284a0a96ae0ba737d2d373c3c54ec7
aoao77 <302719487@qq.com>
2023-07-02 21:55:27 +0800

    reassembler v1.0

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -12,15 +12,50 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) {}
+StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) {
+    reassemble_vec.resize(capacity,'\0');
+    start_idx = 0;
+    end_idx = 0;
+    inorder_idx = 0;
+}
 
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
+    for (size_t i = 0; i < data.size(); i++)
+    {
+        if ((index + i) >= inorder_idx && (index + i) < (start_idx + _capacity))
+        {
+            reassemble_vec[(index + i) % _capacity] = data.at(i);
+        }
+    }
+    // update inorder_idx
+    size_t last_inorder_idx = inorder_idx;
+    for (size_t i = inorder_idx; i < start_idx + _capacity; i++)
+    {
+        if (reassemble_vec[i % _capacity] != '\0')
+        {
+            inorder_idx++;
+        }
+    }
+    // eof
+    if((inorder_idx - last_inorder_idx) == data.size() && eof == true)
+    {
+        _output.end_input();
+    }
 }
 
-size_t StreamReassembler::unassembled_bytes() const { return {}; }
+size_t StreamReassembler::unassembled_bytes() const {
+    size_t res = 0;
+    for (size_t i = inorder_idx; i < start_idx + _capacity; i++)
+    {
+        if (reassemble_vec[i % _capacity] != '\0')
+        {
+            res++;
+        }
+    }
+    return res;
+}
 
-bool StreamReassembler::empty() const { return {}; }
+bool StreamReassembler::empty() const { return unassembled_bytes() == 0; }
